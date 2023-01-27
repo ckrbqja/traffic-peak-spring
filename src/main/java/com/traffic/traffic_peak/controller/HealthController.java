fa048a0ad71ba5f9977adf6e6ee3be37beaf2fe5
chakyubeom <ckrbqja@gmail.com>
2023-01-27 20:15:30 +0900

    rename healthController

--- src/main/java/com/traffic/traffic_peak/controller/HealthController.java
+++ src/main/java/com/traffic/traffic_peak/controller/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class HelloController {
+public class HealthController {
     @GetMapping("/health-check")
     public HealthCheckDto hello() {
         return new HealthCheckDto();
